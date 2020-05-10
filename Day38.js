0479a71541a52d089d69f952dde2baa5dc3c793f
Ineza Bonté Grévy <inezabonte@Inezas-Mac.local>
2020-05-10 12:56:38 +0200

    Some tweaks and a new day

---
+++ Day38.js
@@ -0,0 +1,15 @@
+function persistence(num) {
+    var times = 0;
+    
+    num = num.toString();
+    
+    while (num.length > 1) {
+      times++;
+      num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+    }
+    
+    return times;
+ }
+
+ console.log(persistence(976));
+ console.log(persistence(313));
